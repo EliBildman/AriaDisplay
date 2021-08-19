f41d817d11a833591ac0e8b2d11b4316da95b382
EliBildman <esbildman@gmail.com>
2021-08-19 17:48:45 -0400

    initial router set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import GroupDrawer from './GroupDrawer';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import theme from './theme';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <GroupDrawer>
+          <Switch>
+            <Route exact path='/'>
+              <Redirect to='/events' />
+            </Route>
+            <Route path='/events'>
+              Fuck me
+            </Route>
+            <Route path='/schedule'>
+
+            </Route>
+          </Switch>
+        </GroupDrawer>
+      </ThemeProvider>
     </div>
   );
 }
